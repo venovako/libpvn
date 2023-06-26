23f4e81c32729f1ba408917cbd30171b8e4dc420
Vedran Novakovic <venovako@venovako.eu>
2023-06-26 09:11:15 +0200

    further updates

--- src/clang.mk
+++ src/clang.mk
@@ -7,7 +7,11 @@ CFLAGS=-Og -g -ftrapv
 endif # ?NDEBUG
 CFLAGS += -std=gnu17 -fPIC -fexceptions -ffp-contract=fast -fno-omit-frame-pointer -march=native
 ifeq ($(OS),Linux)
+ifeq ($(ARCH),ppc64le)
+CFLAGS += -fopenmp-simd
+else # !ppc64le
 CFLAGS += -fopenmp
+endif # ?ppc64le
 else # !Linux
 CFLAGS += -fopenmp-simd
 endif # ?Linux
