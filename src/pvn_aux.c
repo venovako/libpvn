0437a8887b645244ab7c21e4ed82f394d9fc0d36
Vedran Novakovic <venovako@venovako.eu>
2023-08-05 22:43:53 +0200

    further updates

--- src/pvn_aux.c
+++ src/pvn_aux.c
@@ -3,14 +3,20 @@
 #ifdef PVN_TEST
 int main(int argc, char *argv[])
 {
-  if (3 != argc) {
-    (void)fprintf(stderr, "%s a b\n", *argv);
+  if (4 != argc) {
+    (void)fprintf(stderr, "%s a b ld\n", *argv);
     return EXIT_FAILURE;
   }
   const size_t a = pvn_atoz(argv[1]);
   const size_t b = pvn_atoz(argv[2]);
   (void)printf("gcd(%zu, %zu) = %zu\n", a, b, pvn_gcd(a, b));
   (void)printf("lcm(%zu, %zu) = %zu\n", a, b, pvn_lcm(a, b));
+  char s[33] = { '\0' };
+  char *e = (char*)NULL;
+  const long double ld = strtold(argv[3], &e);
+  if (e && *e)
+    return EXIT_FAILURE;
+  (void)printf("hexify(%s) = 0x%s\n", argv[3], pvn_hexify(s, &ld, sizeof(ld)));
   return EXIT_SUCCESS;
 }
 #else /* !PVN_TEST */
@@ -54,5 +60,26 @@ size_t pvn_lcm_(const size_t *const a, const size_t *const b)
   assert(a);
   assert(b);
   return pvn_lcm(*a, *b);
+}
+
+char *pvn_hexify(char *const s, const void *const x, const size_t z)
+{
+  if (!s)
+    return s;
+  *s = '\0';
+  if (!x || !z)
+    return s;
+  const unsigned char *const b = (const unsigned char*)x;
+  const unsigned char *e = b + z;
+  char *o = s;
+  do {
+    const unsigned char c = *--e;
+    if (2 != sprintf(o, "%02hhX", c)) {
+      *o = '\0';
+      break;
+    }
+    o += 2;
+  } while (e != b);
+  return s;
 }
 #endif /* ?PVN_TEST */
--- src/pvn_aux.h
+++ src/pvn_aux.h
@@ -15,69 +15,119 @@
 #error PVN_LF64 already defined
 #endif /* ?PVN_LF64 */
 
-static inline int pvn_imin(const int a, const int b)
-{
-  return ((a <= b) ? a : b);
+#ifndef pvn_Tmin
+#define pvn_Tmin(T,t)                              \
+static inline T pvn_##t##min(const T a, const T b) \
+{                                                  \
+  return ((a <= b) ? a : b);                       \
 }
+#else /* pvn_Tmin */
+#error pvn_Tmin already defined
+#endif /* ?pvn_Tmin */
 
-static inline int pvn_imin3(const int a, const int b, const int c)
-{
-  return pvn_imin(pvn_imin(a, b), c);
-}
+pvn_Tmin(int,i)
+pvn_Tmin(long,l)
+pvn_Tmin(long long,ll)
+pvn_Tmin(unsigned,u)
+pvn_Tmin(unsigned long,ul)
+pvn_Tmin(unsigned long long,ull)
+pvn_Tmin(size_t,z)
 
-static inline int pvn_imin4(const int a, const int b, const int c, const int d)
-{
-  return pvn_imin(pvn_imin(a, b), pvn_imin(c, d));
+#ifndef pvn_Tmin3
+#define pvn_Tmin3(T,t)                                         \
+static inline T pvn_##t##min3(const T a, const T b, const T c) \
+{                                                              \
+  return pvn_##t##min(pvn_##t##min(a, b), c);                  \
 }
+#else /* pvn_Tmin3 */
+#error pvn_Tmin3 already defined
+#endif /* ?pvn_Tmin3 */
 
-static inline int pvn_imax(const int a, const int b)
-{
-  return ((a >= b) ? a : b);
-}
+pvn_Tmin3(int,i)
+pvn_Tmin3(long,l)
+pvn_Tmin3(long long,ll)
+pvn_Tmin3(unsigned,u)
+pvn_Tmin3(unsigned long,ul)
+pvn_Tmin3(unsigned long long,ull)
+pvn_Tmin3(size_t,z)
 
-static inline int pvn_imax3(const int a, const int b, const int c)
-{
-  return pvn_imax(pvn_imax(a, b), c);
+#ifndef pvn_Tmin4
+#define pvn_Tmin4(T,t)                                                    \
+static inline T pvn_##t##min4(const T a, const T b, const T c, const T d) \
+{                                                                         \
+  return pvn_##t##min(pvn_##t##min(a, b), pvn_##t##min(c, d));            \
 }
+#else /* pvn_Tmin4 */
+#error pvn_Tmin4 already defined
+#endif /* ?pvn_Tmin4 */
 
-static inline int pvn_imax4(const int a, const int b, const int c, const int d)
-{
-  return pvn_imax(pvn_imax(a, b), pvn_imax(c, d));
-}
+pvn_Tmin4(int,i)
+pvn_Tmin4(long,l)
+pvn_Tmin4(long long,ll)
+pvn_Tmin4(unsigned,u)
+pvn_Tmin4(unsigned long,ul)
+pvn_Tmin4(unsigned long long,ull)
+pvn_Tmin4(size_t,z)
 
-static inline long pvn_lmin(const long a, const long b)
-{
-  return ((a <= b) ? a : b);
+#ifndef pvn_Tmax
+#define pvn_Tmax(T,t)                              \
+static inline T pvn_##t##max(const T a, const T b) \
+{                                                  \
+  return ((a >= b) ? a : b);                       \
 }
+#else /* pvn_Tmax */
+#error pvn_Tmax already defined
+#endif /* ?pvn_Tmax */
 
-static inline long pvn_lmax(const long a, const long b)
-{
-  return ((a >= b) ? a : b);
-}
+pvn_Tmax(int,i)
+pvn_Tmax(long,l)
+pvn_Tmax(long long,ll)
+pvn_Tmax(unsigned,u)
+pvn_Tmax(unsigned long,ul)
+pvn_Tmax(unsigned long long,ull)
+pvn_Tmax(size_t,z)
 
-static inline unsigned pvn_umin(const unsigned a, const unsigned b)
-{
-  return ((a <= b) ? a : b);
+#ifndef pvn_Tmax3
+#define pvn_Tmax3(T,t)                                         \
+static inline T pvn_##t##max3(const T a, const T b, const T c) \
+{                                                              \
+  return pvn_##t##max(pvn_##t##max(a, b), c);                  \
 }
+#else /* pvn_Tmax3 */
+#error pvn_Tmax3 already defined
+#endif /* ?pvn_Tmax3 */
 
-static inline unsigned pvn_umax(const unsigned a, const unsigned b)
-{
-  return ((a >= b) ? a : b);
-}
+pvn_Tmax3(int,i)
+pvn_Tmax3(long,l)
+pvn_Tmax3(long long,ll)
+pvn_Tmax3(unsigned,u)
+pvn_Tmax3(unsigned long,ul)
+pvn_Tmax3(unsigned long long,ull)
+pvn_Tmax3(size_t,z)
 
-static inline size_t pvn_zmin(const size_t a, const size_t b)
-{
-  return ((a <= b) ? a : b);
+#ifndef pvn_Tmax4
+#define pvn_Tmax4(T,t)                                                    \
+static inline T pvn_##t##max4(const T a, const T b, const T c, const T d) \
+{                                                                         \
+  return pvn_##t##max(pvn_##t##max(a, b), pvn_##t##max(c, d));            \
 }
+#else /* pvn_Tmax4 */
+#error pvn_Tmax4 already defined
+#endif /* ?pvn_Tmax4 */
 
-static inline size_t pvn_zmax(const size_t a, const size_t b)
-{
-  return ((a >= b) ? a : b);
-}
+pvn_Tmax4(int,i)
+pvn_Tmax4(long,l)
+pvn_Tmax4(long long,ll)
+pvn_Tmax4(unsigned,u)
+pvn_Tmax4(unsigned long,ul)
+pvn_Tmax4(unsigned long long,ull)
+pvn_Tmax4(size_t,z)
 
 PVN_EXTERN_C size_t pvn_gcd(const size_t a, const size_t b);
 PVN_EXTERN_C size_t pvn_gcd_(const size_t *const a, const size_t *const b);
 PVN_EXTERN_C size_t pvn_lcm(const size_t a, const size_t b);
 PVN_EXTERN_C size_t pvn_lcm_(const size_t *const a, const size_t *const b);
+
+PVN_EXTERN_C char *pvn_hexify(char *const s, const void *const x, const size_t z);
 
 #endif /* !PVN_AUX_H */
