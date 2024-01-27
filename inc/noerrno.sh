a850e268df48eff01c2b1fe9a35225a61de047a3
Vedran Novakovic <venovako@venovako.eu>
2024-01-27 22:53:38 +0100

    further updates

--- inc/noerrno.sh
+++ inc/noerrno.sh
@@ -2,13 +2,13 @@
 if [ `uname -m` = "ppc64le" ]
 then
 	echo ppc64le
-	gcc -O3 -mcpu=native -mtraceback=full -fno-math-errno -W -Wall -c hypotf_noerrno.c
-	gcc -O3 -mcpu=native -mtraceback=full -fno-math-errno -W -Wall -c rsqrtf_noerrno.c
-	gcc -O3 -mcpu=native -mtraceback=full -fno-math-errno -W -Wall -c hypot_noerrno.c
-	gcc -O3 -mcpu=native -mtraceback=full -fno-math-errno -W -Wall -c rsqrt_noerrno.c
+	gcc -O3 -mcpu=native -mtraceback=full -fno-math-errno -W -Wall $* -c hypotf_noerrno.c
+	gcc -O3 -mcpu=native -mtraceback=full -fno-math-errno -W -Wall $* -c rsqrtf_noerrno.c
+	gcc -O3 -mcpu=native -mtraceback=full -fno-math-errno -W -Wall $* -c hypot_noerrno.c
+	gcc -O3 -mcpu=native -mtraceback=full -fno-math-errno -W -Wall $* -c rsqrt_noerrno.c
 else
-	gcc -O3 -march=native -fno-math-errno -W -Wall -c hypotf_noerrno.c
-	gcc -O3 -march=native -fno-math-errno -W -Wall -c rsqrtf_noerrno.c
-	gcc -O3 -march=native -fno-math-errno -W -Wall -c hypot_noerrno.c
-	gcc -O3 -march=native -fno-math-errno -W -Wall -c rsqrt_noerrno.c
+	gcc -O3 -march=native -fno-math-errno -W -Wall $* -c hypotf_noerrno.c
+	gcc -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrtf_noerrno.c
+	gcc -O3 -march=native -fno-math-errno -W -Wall $* -c hypot_noerrno.c
+	gcc -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrt_noerrno.c
 fi
