2771a0201913321331de0c53d1f9cc8641cf0ec2
Vedran Novakovic <venovako@venovako.eu>
2024-03-07 02:27:50 +0100

    further updates

---
+++ src/pvn_ran.c
@@ -0,0 +1,94 @@
+#include "pvn.h"
+
+#ifdef PVN_TEST
+int main(/* int argc, char *argv[] */)
+{
+  const int u = pvn_ran_open_();
+  (void)printf("open=%d\n", pvn_ran_open_());
+  char s[46] = { '\0' };
+  (void)printf("safe_f=%s\n", pvn_stoa(s, pvn_ran_safe_f_(&u)));
+  (void)printf("safe_d=%s\n", pvn_dtoa(s, pvn_ran_safe_(&u)));
+  (void)printf("safe_l=%s\n", pvn_xtoa(s, pvn_ran_safe_l_(&u)));
+  (void)printf("safe_q=%s\n", pvn_qtoa(s, pvn_ran_safe_q_(&u)));
+  (void)printf("close=%d\n", pvn_ran_close_(&u));
+  return EXIT_SUCCESS;
+}
+#else /* !PVN_TEST */
+int pvn_ran_open_()
+{
+  return open("/dev/random", (O_RDONLY | PVN_LF64));
+}
+
+int pvn_ran_close_(const int *const u)
+{
+  assert(u);
+  return close(*u);
+}
+
+float pvn_ran_safe_f_(const int *const u)
+{
+  assert(u);
+  const float rmin = FLT_MIN;
+  const float rmax = (FLT_MAX * 0.25f);
+  float a = FLT_MAX, r = 0.0f;
+  while (!(a >= rmin) || !(a <= rmax)) {
+    if ((ssize_t)sizeof(r) == read(*u, &r, sizeof(r)))
+      a = fabsf(r);
+    else
+      return 0.0f;
+  }
+  return r;
+}
+
+double pvn_ran_safe_(const int *const u)
+{
+  assert(u);
+  const double rmin = DBL_MIN;
+  const double rmax = (DBL_MAX * 0.25);
+  double a = DBL_MAX, r = 0.0;
+  while (!(a >= rmin) || !(a <= rmax)) {
+    if ((ssize_t)sizeof(r) == read(*u, &r, sizeof(r)))
+      a = fabs(r);
+    else
+      return 0.0;
+  }
+  return r;
+}
+
+long double pvn_ran_safe_l_(const int *const u)
+{
+  assert(u);
+  const long double rmin = LDBL_MIN;
+  const long double rmax = (LDBL_MAX * 0.25L);
+  long double a = LDBL_MAX, r = 0.0L;
+  while (!(a >= rmin) || !(a <= rmax)) {
+    if ((ssize_t)sizeof(r) == read(*u, &r, sizeof(r)))
+      a = fabsl(r);
+    else
+      return 0.0L;
+  }
+  return r;
+}
+
+#ifdef PVN_QUADMATH
+__float128 pvn_ran_safe_q_(const int *const u)
+{
+  assert(u);
+  const __float128 rmin = FLT128_MIN;
+  const __float128 rmax = (FLT128_MAX * 0.25q);
+  __float128 a = FLT128_MAX, r = 0.0q;
+  while (!(a >= rmin) || !(a <= rmax)) {
+    if ((ssize_t)sizeof(r) == read(*u, &r, sizeof(r)))
+      a = fabsq(r);
+    else
+      return 0.0q;
+  }
+  return r;
+}
+#else /* !PVN_QUADMATH */
+long double pvn_ran_safe_q_(const int *const u)
+{
+  return pvn_ran_safe_l_(u);
+}
+#endif /* ?PVN_QUADMATH */
+#endif /* ?PVN_TEST */
---
+++ src/pvn_ran.h
@@ -0,0 +1,19 @@
+#ifndef PVN_RAN_H
+#define PVN_RAN_H
+
+#ifndef PVN_H
+#error pvn_ran.h not intended for direct inclusion
+#endif /* !PVN_H */
+
+PVN_EXTERN_C int pvn_ran_open_();
+PVN_EXTERN_C int pvn_ran_close_(const int *const u);
+
+PVN_EXTERN_C float pvn_ran_safe_f_(const int *const u);
+PVN_EXTERN_C double pvn_ran_safe_(const int *const u);
+PVN_EXTERN_C long double pvn_ran_safe_l_(const int *const u);
+#ifdef PVN_QUADMATH
+PVN_EXTERN_C __float128 pvn_ran_safe_q_(const int *const u);
+#else /* !PVN_QUADMATH */
+PVN_EXTERN_C long double pvn_ran_safe_q_(const int *const u);
+#endif /* ?PVN_QUADMATH */
+#endif /* !PVN_RAN_H */
