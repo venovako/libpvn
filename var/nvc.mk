8517c694652fb12ba754473e37d59fdc2c268d1a
Vedran Novakovic <venovako@venovako.eu>
2024-08-21 23:44:27 +0200

    further updates

---
+++ var/nvc.mk
@@ -0,0 +1,16 @@
+ARFLAGS=rsv
+CC=$(COMPILER_PREFIX)nvc$(COMPILER_SUFFIX)
+ifdef NDEBUG
+CFLAGS=-DNDEBUG=$(NDEBUG) -O$(NDEBUG)
+else # DEBUG
+CFLAGS=-O0 -g -Mbounds -Mchkstk
+endif # ?NDEBUG
+CFLAGS += --diag_suppress integer_sign_change -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -c11 -fpic -Kieee -Meh_frame -Mfma -Mframe -Mint128 -Mnodaz -Mnoflushz -Mnofpapprox -Mnofprelaxed -Mno-recip-div -pthread -tp=native -traceback
+# -Minfo -Minstrument -Mm128 -nvmalloc
+ifdef OPENMP
+CFLAGS += -mp
+ifneq ($(OPENMP),true)
+CFLAGS += $(OPENMP)
+endif # !true
+endif # OPENMP
+LDFLAGS=-Wl,-E -ldl -lm
