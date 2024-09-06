e6d747433a081fd921c2a0ee4cd2821212609a26
Vedran Novakovic <venovako@venovako.eu>
2024-09-07 00:47:34 +0200

    further updates

--- src/icx.mk
+++ src/icx.mk
@@ -2,11 +2,11 @@ AR=xiar
 ARFLAGS=-qnoipo -lib rsv
 CC=$(COMPILER_PREFIX)icx$(COMPILER_SUFFIX)
 ifdef NDEBUG
-CFLAGS=-DNDEBUG=$(NDEBUG) -O$(NDEBUG) -fno-math-errno -inline-level=2 -qopt-report=3 -vec-threshold0
+CFLAGS=-DNDEBUG=$(NDEBUG) -O$(NDEBUG) -fno-math-errno -inline-level=2 -qopt-report=3
 else # DEBUG
 CFLAGS=-O0 -g -debug extended -debug inline-debug-info -debug pubnames -debug parallel -ftrapv
 endif # ?NDEBUG
-CFLAGS += -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -std=gnu18 -fPIC -fexceptions -fasynchronous-unwind-tables -fp-model=precise -fp-speculation=safe -fprotect-parens -fma -no-ftz -qopenmp-simd -pthread -fno-omit-frame-pointer -traceback -xHost
+CFLAGS += -D_GNU_SOURCE -D_LARGEFILE64_SOURCE -std=gnu18 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fp-model=precise -fp-speculation=safe -fprotect-parens -fma -no-ftz -fimf-precision=high -mprefer-vector-width=512 -qopenmp-simd -pthread -traceback -vec-threshold0 -xHost
 ifdef OPENMP
 CFLAGS += -qopenmp
 ifneq ($(OPENMP),true)
