38806960caeff69961e92d8c977092722f16fe92
Vedran Novakovic <venovako@venovako.eu>
2024-09-16 04:26:52 +0200

    further updates

--- src/icc.mk
+++ src/icc.mk
@@ -12,7 +12,7 @@ endif # ?NDEBUG
 ifeq ($(OS),Linux)
 CFLAGS += -D_GNU_SOURCE -D_LARGEFILE64_SOURCE
 endif # Linux
-CFLAGS += -std=gnu18 -diag-disable=10397,10441,15519 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fp-model precise -fprotect-parens -fma -no-complex-limited-range -no-ftz -prec-div -prec-sqrt -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -qopenmp-simd -qopt-multi-version-aggressive -qopt-zmm-usage=high -pthread -fno-omit-frame-pointer -traceback -vec-threshold0 -xHost
+CFLAGS += -std=gnu18 -diag-disable=10397,10441,15519 -fPIC -fexceptions -fasynchronous-unwind-tables -fno-omit-frame-pointer -fp-model precise -fprotect-parens -fma -no-complex-limited-range -no-ftz -prec-div -prec-sqrt -qsimd-honor-fp-model -qsimd-serialize-fp-reduction -qopenmp-simd -qopt-multi-version-aggressive -qopt-zmm-usage=high -pthread -traceback -vec-threshold0 -xHost
 ifdef OPENMP
 CFLAGS += -qopenmp
 ifneq ($(OPENMP),true)
