3f3ba2e70891127ac34a38f11c570d427b9a4986
Vedran Novakovic <venovako@venovako.eu>
2025-05-02 19:43:28 +0200

    further updates

--- inc/errno.sh
+++ inc/errno.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 MACHINE=`uname -m`
-echo "MACHINE=$MACHINE"
+if [ "$GCC" = "" ]
+then
+	GCC=gcc
+fi
+echo "MACHINE=$MACHINE; GCC=$GCC"
 if [ "$MACHINE" = "ppc64le" ]
 then
-	gcc -DCORE_MATH_SUPPORT_ERRNO -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c hypotf_noerrno.c -o hypotf.o
-	gcc -DCORE_MATH_SUPPORT_ERRNO -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c rsqrtf_noerrno.c -o rsqrtf.o
-	gcc -DCORE_MATH_SUPPORT_ERRNO -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c hypot_noerrno.c -o hypot.o
-	gcc -DCORE_MATH_SUPPORT_ERRNO -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c rsqrt_noerrno.c -o rsqrt.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c hypotf_noerrno.c -o hypotf.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c rsqrtf_noerrno.c -o rsqrtf.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c hypot_noerrno.c -o hypot.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c rsqrt_noerrno.c -o rsqrt.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -mcpu=native -mpower8-fusion -mtraceback=full -W -Wall $* -c rsqrtq_noerrno.c -o rsqrtq.o
 else
-	gcc -DCORE_MATH_SUPPORT_ERRNO -Og -ggdb3 -march=native -W -Wall $* -c hypotf_noerrno.c -o hypotf.o
-	gcc -DCORE_MATH_SUPPORT_ERRNO -Og -ggdb3 -march=native -W -Wall $* -c rsqrtf_noerrno.c -o rsqrtf.o
-	gcc -DCORE_MATH_SUPPORT_ERRNO -Og -ggdb3 -march=native -W -Wall $* -c hypot_noerrno.c -o hypot.o
-	gcc -DCORE_MATH_SUPPORT_ERRNO -Og -ggdb3 -march=native -W -Wall $* -c rsqrt_noerrno.c -o rsqrt.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -march=native -W -Wall $* -c hypotf_noerrno.c -o hypotf.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -march=native -W -Wall $* -c rsqrtf_noerrno.c -o rsqrtf.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -march=native -W -Wall $* -c hypot_noerrno.c -o hypot.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -march=native -W -Wall $* -c rsqrt_noerrno.c -o rsqrt.o
+	$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -march=native -W -Wall $* -c rsqrtq_noerrno.c -o rsqrtq.o
 	if [ "$MACHINE" = "x86_64" -o "$MACHINE" = "amd64" ]
 	then
-		gcc -Og -ggdb3 -march=native -W -Wall $* -c hypotl_noerrno.c -o hypotl.o
-		gcc -Og -ggdb3 -march=native -W -Wall $* -c rsqrtl_noerrno.c -o rsqrtl.o
+		$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -march=native -W -Wall $* -c hypotl_noerrno.c -o hypotl.o
+		$GCC -DCORE_MATH_SUPPORT_ERRNO -I../src -Og -ggdb3 -march=native -W -Wall $* -c rsqrtl_noerrno.c -o rsqrtl.o
 	fi
 fi
--- inc/noerrno.sh
+++ inc/noerrno.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 MACHINE=`uname -m`
-echo "MACHINE=$MACHINE"
+if [ "$GCC" = "" ]
+then
+	GCC=gcc
+fi
+echo "MACHINE=$MACHINE; GCC=$GCC"
 if [ "$MACHINE" = "ppc64le" ]
 then
-	gcc -DNDEBUG -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c hypotf_noerrno.c
-	gcc -DNDEBUG -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c rsqrtf_noerrno.c
-	gcc -DNDEBUG -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c hypot_noerrno.c
-	gcc -DNDEBUG -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c rsqrt_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c hypotf_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c rsqrtf_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c hypot_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c rsqrt_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -mcpu=native -mpower8-fusion -mtraceback=full -fno-math-errno -W -Wall $* -c rsqrtq_noerrno.c
 else
-	gcc -DNDEBUG -O3 -march=native -fno-math-errno -W -Wall $* -c hypotf_noerrno.c
-	gcc -DNDEBUG -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrtf_noerrno.c
-	gcc -DNDEBUG -O3 -march=native -fno-math-errno -W -Wall $* -c hypot_noerrno.c
-	gcc -DNDEBUG -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrt_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -march=native -fno-math-errno -W -Wall $* -c hypotf_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrtf_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -march=native -fno-math-errno -W -Wall $* -c hypot_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrt_noerrno.c
+	$GCC -DNDEBUG -I../src -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrtq_noerrno.c
 	if [ "$MACHINE" = "x86_64" -o "$MACHINE" = "amd64" ]
 	then
-		gcc -DNDEBUG -O3 -march=native -fno-math-errno -W -Wall $* -c hypotl_noerrno.c
-		gcc -DNDEBUG -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrtl_noerrno.c
+		$GCC -DNDEBUG -I../src -O3 -march=native -fno-math-errno -W -Wall $* -c hypotl_noerrno.c
+		$GCC -DNDEBUG -I../src -O3 -march=native -fno-math-errno -W -Wall $* -c rsqrtl_noerrno.c
 	fi
 fi
