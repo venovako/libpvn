0228bd483a16d5442d07d7593440b82a1a0a3115
Vedran Novakovic <venovako@venovako.eu>
2025-06-20 00:37:59 +0200

    further updates

--- inc/rsqrtq_noerrno.c
+++ inc/rsqrtq_noerrno.c
@@ -262,7 +262,7 @@ __float128 cr_rsqrtq(__float128 x){
 #ifdef CORE_MATH_SUPPORT_ERRNO
 	errno = ERANGE; // pole error
 #endif
-	return 1.0f128/0.0f128; // x = +0
+	return 1.0q/0.0q; // x = +0
       }
     }
     e = 1 - ns;
@@ -274,7 +274,7 @@ __float128 cr_rsqrtq(__float128 x){
 #ifdef CORE_MATH_SUPPORT_ERRNO
       errno = ERANGE; // pole error
 #endif
-      return -1.0f128/0.0f128;
+      return -1.0q/0.0q;
     }
     if(u.a==(u128)0x7fff<<112) return 0; // x = +Inf
     if(u.a<=(u128)0xffff<<112 && u.a>(u128)0x8000<<112){ // -inf <= x < -0
