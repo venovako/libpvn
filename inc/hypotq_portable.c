ec9a3c7f1b6fc80f3c3e9fd28b77462229ac466c
Vedran Novakovic <venovako@venovako.eu>
2025-07-30 20:27:37 +0200

    further updates

--- inc/hypotq_portable.c
+++ inc/hypotq_portable.c
@@ -111,8 +111,8 @@ static inline u128 addUU(u64 *c, u128 _a, u128 _b){
   b128u128_u a, b;
   a.a = _a;
   b.a = _b;
-  a.b[0] = __builtin_addcl(a.b[0], b.b[0],  0, c);
-  a.b[1] = __builtin_addcl(a.b[1], b.b[1], *c, c);
+  a.b[0] = __builtin_addcl(a.b[0], b.b[0],  0, (unsigned long*)c);
+  a.b[1] = __builtin_addcl(a.b[1], b.b[1], *c, (unsigned long*)c);
   return a.a;
 }
 
