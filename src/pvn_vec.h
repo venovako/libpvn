819cced2bce04c6a6c0f18c0e6ce5b953e735966
Vedran Novaković <venovako@venovako.eu>
2025-07-31 04:39:27 +0200

    further updates

--- src/pvn_vec.h
+++ src/pvn_vec.h
@@ -90,8 +90,8 @@ static inline __m256 pvn_v8s_hypot(register const __m256 x, register const __m25
 {
   register const __m256 z = _mm256_set1_ps(-0.0f);
   register const __m256 o = _mm256_set1_ps(1.0f);
-  register const __m256 X = _mm256_xor_ps(x, z);
-  register const __m256 Y = _mm256_xor_ps(y, z);
+  register const __m256 X = _mm256_andnot_ps(z, x);
+  register const __m256 Y = _mm256_andnot_ps(z, y);
   register const __m256 m = _mm256_min_ps(X, Y);
   register const __m256 M = _mm256_max_ps(X, Y);
   register const __m256 q = _mm256_div_ps(m, M);
@@ -106,8 +106,8 @@ static inline __m256d pvn_v4d_hypot(register const __m256d x, register const __m
 {
   register const __m256d z = _mm256_set1_pd(-0.0);
   register const __m256d o = _mm256_set1_pd(1.0);
-  register const __m256d X = _mm256_xor_pd(x, z);
-  register const __m256d Y = _mm256_xor_pd(y, z);
+  register const __m256d X = _mm256_andnot_pd(z, x);
+  register const __m256d Y = _mm256_andnot_pd(z, y);
   register const __m256d m = _mm256_min_pd(X, Y);
   register const __m256d M = _mm256_max_pd(X, Y);
   register const __m256d q = _mm256_div_pd(m, M);
@@ -124,8 +124,8 @@ static inline __m512 pvn_v16s_hypot(register const __m512 x, register const __m5
 {
   register const __m512 z = _mm512_set1_ps(-0.0f);
   register const __m512 o = _mm512_set1_ps(1.0f);
-  register const __m512 X = _mm512_castsi512_ps(_mm512_xor_epi32(_mm512_castps_si512(x), _mm512_castps_si512(z)));
-  register const __m512 Y = _mm512_castsi512_ps(_mm512_xor_epi32(_mm512_castps_si512(y), _mm512_castps_si512(z)));
+  register const __m512 X = _mm512_castsi512_ps(_mm512_andnot_epi32(_mm512_castps_si512(z), _mm512_castps_si512(x)));
+  register const __m512 Y = _mm512_castsi512_ps(_mm512_andnot_epi32(_mm512_castps_si512(z), _mm512_castps_si512(y)));
   register const __m512 m = _mm512_min_ps(X, Y);
   register const __m512 M = _mm512_max_ps(X, Y);
   register const __m512 q = _mm512_div_ps(m, M);
@@ -140,8 +140,8 @@ static inline __m512d pvn_v8d_hypot(register const __m512d x, register const __m
 {
   register const __m512d z = _mm512_set1_pd(-0.0);
   register const __m512d o = _mm512_set1_pd(1.0);
-  register const __m512d X = _mm512_castsi512_pd(_mm512_xor_epi64(_mm512_castpd_si512(x), _mm512_castpd_si512(z)));
-  register const __m512d Y = _mm512_castsi512_pd(_mm512_xor_epi64(_mm512_castpd_si512(y), _mm512_castpd_si512(z)));
+  register const __m512d X = _mm512_castsi512_pd(_mm512_andnot_epi64(_mm512_castpd_si512(z), _mm512_castpd_si512(x)));
+  register const __m512d Y = _mm512_castsi512_pd(_mm512_andnot_epi64(_mm512_castpd_si512(z), _mm512_castpd_si512(y)));
   register const __m512d m = _mm512_min_pd(X, Y);
   register const __m512d M = _mm512_max_pd(X, Y);
   register const __m512d q = _mm512_div_pd(m, M);
