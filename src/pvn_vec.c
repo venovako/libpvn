37e6bc9e50b86b467026ab0d7989b2c3806dac69
Vedran Novakovic <venovako@venovako.eu>
2025-08-01 04:06:37 +0200

    further updates

--- src/pvn_vec.c
+++ src/pvn_vec.c
@@ -1,11 +1,46 @@
 #include "pvn.h"
 
 #ifdef PVN_TEST
-int main(/* int argc, char *argv[] */)
+int main(int argc, char *argv[])
 {
+  if (argc > 3) {
+    (void)fprintf(stderr, "%s [n [seed]]\n", *argv);
+    return EXIT_FAILURE;
+  }
   (void)printf("PVN_VECLEN = %u\n", PVN_VECLEN);
   (void)printf("PVN_SAFELEN(float) = %zu\n", PVN_SAFELEN(float));
   (void)printf("PVN_SAFELEN(double) = %zu\n", PVN_SAFELEN(double));
+#if (defined(__AVX__) && defined(__FMA__))
+  if (argc > 1) {
+    const size_t n = pvn_atoz(argv[1]);
+    if (!n)
+      return EXIT_FAILURE;
+    double *const x = (double*)aligned_alloc(64u, n * sizeof(double));
+    if (!x)
+      return EXIT_FAILURE;
+#ifndef _WIN32
+    if (argc > 2)
+      srand48(atol(argv[2]));
+    for (size_t i = n; i; )
+      x[--i] = drand48();
+#endif /* !_WIN32 */
+    (void)printf("pvn_v4d_nrmf=");
+    (void)fflush(stdout);
+    long long t = pvn_time_mono_ns();
+    double f = PVN_FABI(pvn_v4d_nrmf,PVN_V4D_NRMF)(&n, x);
+    t = pvn_time_mono_ns() - t;
+    (void)printf("%#.17e in %lld ns\n", f, t);
+#ifdef __AVX512F__
+    (void)printf("pvn_v8d_nrmf=");
+    (void)fflush(stdout);
+    t = pvn_time_mono_ns();
+    f = PVN_FABI(pvn_v8d_nrmf,PVN_V8D_NRMF)(&n, x);
+    t = pvn_time_mono_ns() - t;
+    (void)printf("%#.17e in %lld ns\n", f, t);
+#endif /* __AVX512F__ */
+    free(x);
+  }
+#endif /* __AVX__ && __FMA__ */
   return EXIT_SUCCESS;
 }
 #else /* !PVN_TEST */
@@ -13,4 +48,36 @@ unsigned PVN_FABI(pvn_vec_len,PVN_VEC_LEN)()
 {
   return (PVN_VECLEN);
 }
+#if (defined(__AVX__) && defined(__FMA__))
+double PVN_FABI(pvn_v4d_nrmf,PVN_V4D_NRMF)(const size_t *const n, const double *const x)
+{
+  PVN_ASSERT(n);
+  PVN_ASSERT(x);
+  if (*n & 3u)
+    return -1.0;
+  if (!*n)
+    return -0.0;
+  const size_t m = *n;
+  register __m256d f = _mm256_setzero_pd();
+  for (size_t i = 0u; i < m; i += 4u)
+    f = pvn_v4d_hypot(f, _mm256_load_pd(x + i));
+  return pvn_v4d_hypot_red(f);
+}
+#ifdef __AVX512F__
+double PVN_FABI(pvn_v8d_nrmf,PVN_V8D_NRMF)(const size_t *const n, const double *const x)
+{
+  PVN_ASSERT(n);
+  PVN_ASSERT(x);
+  if (*n & 7u)
+    return -1.0;
+  if (!*n)
+    return -0.0;
+  const size_t m = *n;
+  register __m512d f = _mm512_setzero_pd();
+  for (size_t i = 0u; i < m; i += 8u)
+    f = pvn_v8d_hypot(f, _mm512_load_pd(x + i));
+  return pvn_v8d_hypot_red(f);
+}
+#endif /* __AVX512F__ */
+#endif /* __AVX__ && __FMA__ */
 #endif /* ?PVN_TEST */
--- src/pvn_vec.h
+++ src/pvn_vec.h
@@ -106,9 +106,70 @@ static inline __float128 pvn_v1q_hypot(const __float128 x, const __float128 y)
 #if (defined(__AVX__) && defined(__FMA__))
 #include <immintrin.h>
 
+static inline __m128 pvn_v4s_rsqrt(register const __m128 x)
+{
+  return _mm_div_ps(_mm_set1_ps(1.0f), _mm_sqrt_ps(x)); //_mm_invsqrt_ps(x)
+}
+
+static inline float pvn_v4s_hypot_red(register const __m128 x)
+{
+  alignas(16) float v[4];
+  _mm_store_ps(v, x);
+  return pvn_v1s_hypot(pvn_v1s_hypot(v[0], v[2]), pvn_v1s_hypot(v[1], v[3]));
+}
+
+static inline __m128 pvn_v4s_hypot(register const __m128 x, register const __m128 y)
+{
+  register const __m128 z = _mm_set1_ps(-0.0f);
+  register const __m128 o = _mm_set1_ps(1.0f);
+  register const __m128 X = _mm_andnot_ps(z, x);
+  register const __m128 Y = _mm_andnot_ps(z, y);
+  register const __m128 m = _mm_min_ps(X, Y);
+  register const __m128 M = _mm_max_ps(X, Y);
+  register const __m128 q = _mm_div_ps(m, M);
+  register const __m128 Q = _mm_max_ps(q, z);
+  register const __m128 S = _mm_fmadd_ps(Q, Q, o);
+  register const __m128 s = _mm_sqrt_ps(S);
+  register const __m128 h = _mm_mul_ps(M, s);
+  return h;
+}
+
+static inline __m128d pvn_v2d_rsqrt(register const __m128d x)
+{
+  return _mm_div_pd(_mm_set1_pd(1.0), _mm_sqrt_pd(x)); //_mm_invsqrt_pd(x)
+}
+
+static inline double pvn_v2d_hypot_red(register const __m128d x)
+{
+  alignas(16) double v[2];
+  _mm_store_pd(v, x);
+  return pvn_v1d_hypot(v[0], v[1]);
+}
+
+static inline __m128d pvn_v2d_hypot(register const __m128d x, register const __m128d y)
+{
+  register const __m128d z = _mm_set1_pd(-0.0);
+  register const __m128d o = _mm_set1_pd(1.0);
+  register const __m128d X = _mm_andnot_pd(z, x);
+  register const __m128d Y = _mm_andnot_pd(z, y);
+  register const __m128d m = _mm_min_pd(X, Y);
+  register const __m128d M = _mm_max_pd(X, Y);
+  register const __m128d q = _mm_div_pd(m, M);
+  register const __m128d Q = _mm_max_pd(q, z);
+  register const __m128d S = _mm_fmadd_pd(Q, Q, o);
+  register const __m128d s = _mm_sqrt_pd(S);
+  register const __m128d h = _mm_mul_pd(M, s);
+  return h;
+}
+
 static inline __m256 pvn_v8s_rsqrt(register const __m256 x)
 {
   return _mm256_div_ps(_mm256_set1_ps(1.0f), _mm256_sqrt_ps(x)); //_mm256_invsqrt_ps(x)
+}
+
+static inline float pvn_v8s_hypot_red(register const __m256 x)
+{
+  return pvn_v4s_hypot_red(pvn_v4s_hypot(_mm256_extractf128_ps(x, 0), _mm256_extractf128_ps(x, 1)));
 }
 
 static inline __m256 pvn_v8s_hypot(register const __m256 x, register const __m256 y)
@@ -130,6 +191,11 @@ static inline __m256 pvn_v8s_hypot(register const __m256 x, register const __m25
 static inline __m256d pvn_v4d_rsqrt(register const __m256d x)
 {
   return _mm256_div_pd(_mm256_set1_pd(1.0), _mm256_sqrt_pd(x)); //_mm256_invsqrt_pd(x)
+}
+
+static inline double pvn_v4d_hypot_red(register const __m256d x)
+{
+  return pvn_v2d_hypot_red(pvn_v2d_hypot(_mm256_extractf128_pd(x, 0), _mm256_extractf128_pd(x, 1)));
 }
 
 static inline __m256d pvn_v4d_hypot(register const __m256d x, register const __m256d y)
@@ -154,6 +220,11 @@ static inline __m512 pvn_v16s_rsqrt(register const __m512 x)
   return _mm512_div_ps(_mm512_set1_ps(1.0f), _mm512_sqrt_ps(x)); //_mm512_invsqrt_ps(x)
 }
 
+static inline float pvn_v16s_hypot_red(register const __m512 x)
+{
+  return pvn_v8s_hypot_red(pvn_v8s_hypot(_mm512_castpd_ps(_mm512_extractf64x4_pd(_mm512_castps_pd(x), 0)), _mm512_castpd_ps(_mm512_extractf64x4_pd(_mm512_castps_pd(x), 1))));
+}
+
 static inline __m512 pvn_v16s_hypot(register const __m512 x, register const __m512 y)
 {
   register const __m512 z = _mm512_set1_ps(-0.0f);
@@ -175,6 +246,11 @@ static inline __m512d pvn_v8d_rsqrt(register const __m512d x)
   return _mm512_div_pd(_mm512_set1_pd(1.0), _mm512_sqrt_pd(x)); //_mm512_invsqrt_pd(x)
 }
 
+static inline double pvn_v8d_hypot_red(register const __m512d x)
+{
+  return pvn_v4d_hypot_red(pvn_v4d_hypot(_mm512_extractf64x4_pd(x, 0), _mm512_extractf64x4_pd(x, 1)));
+}
+
 static inline __m512d pvn_v8d_hypot(register const __m512d x, register const __m512d y)
 {
   register const __m512d z = _mm512_set1_pd(-0.0);
@@ -194,5 +270,11 @@ static inline __m512d pvn_v8d_hypot(register const __m512d x, register const __m
 #endif /* __AVX__ && __FMA__ */
 
 PVN_EXTERN_C unsigned PVN_FABI(pvn_vec_len,PVN_VEC_LEN)();
+#if (defined(__AVX__) && defined(__FMA__))
+PVN_EXTERN_C double PVN_FABI(pvn_v4d_nrmf,PVN_V4D_NRMF)(const size_t *const n, const double *const x);
+#ifdef __AVX512F__
+PVN_EXTERN_C double PVN_FABI(pvn_v8d_nrmf,PVN_V8D_NRMF)(const size_t *const n, const double *const x);
+#endif /* __AVX512F__ */
+#endif /* __AVX__ && __FMA__ */
 
 #endif /* !PVN_VEC_H */
