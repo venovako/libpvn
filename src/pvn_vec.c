0ff7bbf9f3dcecd4b464cf5fb1ab505e76d12dcc
Vedran Novakovic <venovako@venovako.eu>
2025-08-01 05:07:34 +0200

    further updates

--- src/pvn_vec.c
+++ src/pvn_vec.c
@@ -69,12 +69,18 @@ unsigned PVN_FABI(pvn_vec_len,PVN_VEC_LEN)()
 
 double PVN_FABI(pvn_v1d_nrmf,PVN_V1D_NRMF)(const size_t *const n, const double *const x)
 {
+#ifdef _OPENMP
+#pragma omp declare reduction(h1d:double:omp_out=pvn_v1d_hypot(omp_out,omp_in)) initializer(omp_priv=0.0)
+#endif /* _OPENMP */
   PVN_ASSERT(n);
   PVN_ASSERT(x);
   if (!*n)
     return -0.0;
   const size_t m = *n;
   double f = 0.0;
+#ifdef _OPENMP
+#pragma omp parallel for default(none) shared(m,x) reduction(h1d:f)
+#endif /* _OPENMP */
   for (size_t i = 0u; i < m; ++i)
     f = pvn_v1d_hypot(f, x[i]);
   return f;
@@ -82,12 +88,18 @@ double PVN_FABI(pvn_v1d_nrmf,PVN_V1D_NRMF)(const size_t *const n, const double *
 
 double PVN_FABI(pvn_crd_nrmf,PVN_CRD_NRMF)(const size_t *const n, const double *const x)
 {
+#ifdef _OPENMP
+#pragma omp declare reduction(hcd:double:omp_out=hypot(omp_out,omp_in)) initializer(omp_priv=0.0)
+#endif /* _OPENMP */
   PVN_ASSERT(n);
   PVN_ASSERT(x);
   if (!*n)
     return -0.0;
   const size_t m = *n;
   double f = 0.0;
+#ifdef _OPENMP
+#pragma omp parallel for default(none) shared(m,x) reduction(hcd:f)
+#endif /* _OPENMP */
   for (size_t i = 0u; i < m; ++i)
     f = hypot(f, x[i]);
   return f;
@@ -95,46 +107,64 @@ double PVN_FABI(pvn_crd_nrmf,PVN_CRD_NRMF)(const size_t *const n, const double *
 #if (defined(__AVX__) && defined(__FMA__))
 double PVN_FABI(pvn_v2d_nrmf,PVN_V2D_NRMF)(const size_t *const n, const double *const x)
 {
+#ifdef _OPENMP
+#pragma omp declare reduction(h2d:__m128d:omp_out=pvn_v2d_hypot(omp_out,omp_in)) initializer(omp_priv=_mm_setzero_pd())
+#endif /* _OPENMP */
   PVN_ASSERT(n);
   PVN_ASSERT(x);
   if (*n & 1u)
     return -1.0;
   if (!*n)
     return -0.0;
-  const size_t m = *n;
+  const size_t m = (*n >> 1u);
   register __m128d f = _mm_setzero_pd();
-  for (size_t i = 0u; i < m; i += 2u)
-    f = pvn_v2d_hypot(f, _mm_load_pd(x + i));
+#ifdef _OPENMP
+#pragma omp parallel for default(none) shared(m,x) reduction(h2d:f)
+#endif /* _OPENMP */
+  for (size_t i = 0u; i < m; ++i)
+    f = pvn_v2d_hypot(f, _mm_load_pd(x + (i << 1u)));
   return pvn_v2d_hypot_red(f);
 }
 
 double PVN_FABI(pvn_v4d_nrmf,PVN_V4D_NRMF)(const size_t *const n, const double *const x)
 {
+#ifdef _OPENMP
+#pragma omp declare reduction(h4d:__m256d:omp_out=pvn_v4d_hypot(omp_out,omp_in)) initializer(omp_priv=_mm256_setzero_pd())
+#endif /* _OPENMP */
   PVN_ASSERT(n);
   PVN_ASSERT(x);
   if (*n & 3u)
     return -1.0;
   if (!*n)
     return -0.0;
-  const size_t m = *n;
+  const size_t m = (*n >> 2u);
   register __m256d f = _mm256_setzero_pd();
-  for (size_t i = 0u; i < m; i += 4u)
-    f = pvn_v4d_hypot(f, _mm256_load_pd(x + i));
+#ifdef _OPENMP
+#pragma omp parallel for default(none) shared(m,x) reduction(h4d:f)
+#endif /* _OPENMP */
+  for (size_t i = 0u; i < m; ++i)
+    f = pvn_v4d_hypot(f, _mm256_load_pd(x + (i << 2u)));
   return pvn_v4d_hypot_red(f);
 }
 #ifdef __AVX512F__
 double PVN_FABI(pvn_v8d_nrmf,PVN_V8D_NRMF)(const size_t *const n, const double *const x)
 {
+#ifdef _OPENMP
+#pragma omp declare reduction(h8d:__m512d:omp_out=pvn_v8d_hypot(omp_out,omp_in)) initializer(omp_priv=_mm512_setzero_pd())
+#endif /* _OPENMP */
   PVN_ASSERT(n);
   PVN_ASSERT(x);
   if (*n & 7u)
     return -1.0;
   if (!*n)
     return -0.0;
-  const size_t m = *n;
+  const size_t m = (*n >> 3u);
   register __m512d f = _mm512_setzero_pd();
-  for (size_t i = 0u; i < m; i += 8u)
-    f = pvn_v8d_hypot(f, _mm512_load_pd(x + i));
+#ifdef _OPENMP
+#pragma omp parallel for default(none) shared(m,x) reduction(h8d:f)
+#endif /* _OPENMP */
+  for (size_t i = 0u; i < m; ++i)
+    f = pvn_v8d_hypot(f, _mm512_load_pd(x + (i << 3u)));
   return pvn_v8d_hypot_red(f);
 }
 #endif /* __AVX512F__ */
