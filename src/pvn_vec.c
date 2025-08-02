f52e8df8c9c430d96847ddeae2624e305a651081
Vedran Novakovic <venovako@venovako.eu>
2025-08-02 03:37:01 +0200

    further updates

--- src/pvn_vec.c
+++ src/pvn_vec.c
@@ -5,14 +5,45 @@ extern double PVN_FABI(dnrm2,DNRM2)(const size_t *const n, const double *const x
 
 static double PVN_FABI(pvn_lad_nrmf,PVN_LAD_NRMF)(const size_t *const n, const double *const x)
 {
+  PVN_ASSERT(n);
+  PVN_ASSERT(x);
+  if (!*n)
+    return -0.0;
   const int64_t incx = 1u;
   return PVN_FABI(dnrm2,DNRM2)(n, x, &incx);
 }
 
+#ifdef PVN_MPFR
+static double PVN_FABI(pvn_mpd_nrmf,PVN_MPD_NRMF)(const size_t *const n, const double *const x)
+{
+  PVN_ASSERT(n);
+  PVN_ASSERT(x);
+  if (!*n)
+    return -0.0;
+  const size_t m = *n;
+  mpfr_t mf, mx;
+  (void)mpfr_init_set_d(mf, 0.0, MPFR_RNDN);
+  (void)mpfr_init_set_d(mx, 0.0, MPFR_RNDN);
+  for (size_t i = 0u; i < m; ++i) {
+    (void)mpfr_set_d(mx, x[i], MPFR_RNDN);
+    (void)mpfr_hypot(mf, mf, mx, MPFR_RNDN);
+  }
+  const double f = mpfr_get_d(mf, MPFR_RNDN);
+  mpfr_clear(mx);
+  mpfr_clear(mf);
+  return f;
+}
+#endif /* PVN_MPFR */
+
+static double erelerr(const double e, const double f)
+{
+  return ((e == 0.0) ? -0.0 : (fabs(e - f) / scalbn(fabs(e), -53)));
+}
+
 int main(int argc, char *argv[])
 {
-  if (argc > 3) {
-    (void)fprintf(stderr, "%s [n [seed]]\n", *argv);
+  if (argc > 4) {
+    (void)fprintf(stderr, "%s [n [seed [prec]]]\n", *argv);
     return EXIT_FAILURE;
   }
   (void)printf("PVN_VECLEN = %u\n", PVN_VECLEN);
@@ -23,45 +54,62 @@ int main(int argc, char *argv[])
     const size_t n = pvn_atoz(argv[1]);
     if (!n)
       return EXIT_FAILURE;
-    double *const x = (double*)aligned_alloc(PVN_FABI(pvn_pagesize,PVN_PAGESIZE)(), n * sizeof(double));
+    double *const x = (double*)aligned_alloc(64u, n * sizeof(double));
     if (!x)
       return EXIT_FAILURE;
-    if (argc > 2)
+    if (argc > 2) // e.g., the result of `echo "$RANDOM * $RANDOM" | bc`
       srand48(atol(argv[2]));
     for (size_t i = 0u; i < n; ++i)
       x[i] = drand48();
+#ifdef PVN_MPFR
+    mpfr_rnd_t rnd = MPFR_RNDN;
+    mpfr_prec_t prec = ((argc > 3) ? atol(argv[3]) : 2048l);
+    mpfr_exp_t emin = __MPFR_EXP_INVALID, emax = __MPFR_EXP_INVALID;
+    if (PVN_FABI(pvn_mpfr_start,PVN_MPFR_START)(&rnd, &prec, &emin, &emax))
+      return EXIT_FAILURE;
+#endif /* PVN_MPFR */
     long long t = 0ll;
+#ifdef PVN_MPFR
+    (void)printf("pvn_mpd_nrmf=");
+    (void)fflush(stdout);
+    t = pvn_time_mono_ns();
+    const double e = PVN_FABI(pvn_mpd_nrmf,PVN_MPD_NRMF)(&n, x);
+    t = pvn_time_mono_ns() - t;
+    (void)printf("%# .17e relerr/ε %# .17e in %21lld ns\n", e, -0.0, t);
+#else /* !PVN_MPFR */
+    const double e = 0.0;
+#endif /* ?PVN_MPFR */
     double f = 0.0;
     (void)printf("pvn_lad_nrmf=");
     (void)fflush(stdout);
     t = pvn_time_mono_ns();
     f = PVN_FABI(pvn_lad_nrmf,PVN_LAD_NRMF)(&n, x);
     t = pvn_time_mono_ns() - t;
-    (void)printf("%#.17e in %21lld ns\n", f, t);
+    (void)printf("%# .17e relerr/ε %# .17e in %21lld ns\n", f, erelerr(e, f), t);
     (void)printf("pvn_crd_nrmf=");
     (void)fflush(stdout);
     t = pvn_time_mono_ns();
     f = PVN_FABI(pvn_crd_nrmf,PVN_CRD_NRMF)(&n, x);
     t = pvn_time_mono_ns() - t;
-    (void)printf("%#.17e in %21lld ns\n", f, t);
+    (void)printf("%# .17e relerr/ε %# .17e in %21lld ns\n", f, erelerr(e, f), t);
     (void)printf("pvn_v1d_nrmf=");
     (void)fflush(stdout);
     t = pvn_time_mono_ns();
     f = PVN_FABI(pvn_v1d_nrmf,PVN_V1D_NRMF)(&n, x);
     t = pvn_time_mono_ns() - t;
-    (void)printf("%#.17e in %21lld ns\n", f, t);
+    (void)printf("%# .17e relerr/ε %# .17e in %21lld ns\n", f, erelerr(e, f), t);
     (void)printf("pvn_v2d_nrmf=");
     (void)fflush(stdout);
     t = pvn_time_mono_ns();
     f = PVN_FABI(pvn_v2d_nrmf,PVN_V2D_NRMF)(&n, x);
     t = pvn_time_mono_ns() - t;
-    (void)printf("%#.17e in %21lld ns\n", f, t);
+    (void)printf("%# .17e relerr/ε %# .17e in %21lld ns\n", f, erelerr(e, f), t);
     (void)printf("pvn_v4d_nrmf=");
     (void)fflush(stdout);
     t = pvn_time_mono_ns();
     f = PVN_FABI(pvn_v4d_nrmf,PVN_V4D_NRMF)(&n, x);
     t = pvn_time_mono_ns() - t;
-    (void)printf("%#.17e in %21lld ns\n", f, t);
+    (void)printf("%# .17e relerr/ε %# .17e in %21lld ns\n", f, erelerr(e, f), t);
 #ifdef __AVX512F__
     (void)printf("pvn_v8d_nrmf=");
     (void)fflush(stdout);
@@ -70,6 +118,9 @@ int main(int argc, char *argv[])
     t = pvn_time_mono_ns() - t;
     (void)printf("%#.17e in %21lld ns\n", f, t);
 #endif /* __AVX512F__ */
+#ifdef PVN_MPFR
+    (void)PVN_FABI(pvn_mpfr_stop,PVN_MPFR_STOP)();
+#endif /* PVN_MPFR */
     free(x);
   }
 #endif /* __AVX__ && __FMA__ && !_WIN32 */
