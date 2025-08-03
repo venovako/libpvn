c55ebcada65e4ea3b9b36450e79b6190a21df43a
Vedran Novakovic <venovako@venovako.eu>
2025-08-03 05:11:06 +0200

    further updates

--- src/pvn_fmt.c
+++ src/pvn_fmt.c
@@ -31,6 +31,17 @@ int main(/* int argc, char *argv[] */)
   (void)printf("FLT128_MAX     =%s\n", pvn_qtoa(s, FLT128_MAX));
   (void)printf("FLT128_MAX_EXP =%6d\n", FLT128_MAX_EXP);
 #endif /* PVN_QUADMATH */
+#ifndef _WIN32
+  (void)printf(PVN_ANSI_BLACK "Black" PVN_ANSI_CLEAR "\n");
+  (void)printf(PVN_ANSI_RED "Red" PVN_ANSI_CLEAR "\n");
+  (void)printf(PVN_ANSI_GREEN "Green" PVN_ANSI_CLEAR "\n");
+  (void)printf(PVN_ANSI_YELLOW "Yellow" PVN_ANSI_CLEAR "\n");
+  (void)printf(PVN_ANSI_BLUE "Blue" PVN_ANSI_CLEAR "\n");
+  (void)printf(PVN_ANSI_MAGENTA "Magenta" PVN_ANSI_CLEAR "\n");
+  (void)printf(PVN_ANSI_CYAN "Cyan" PVN_ANSI_CLEAR "\n");
+  (void)printf(PVN_ANSI_WHITE "White" PVN_ANSI_CLEAR "\n");
+  (void)printf("Foreground\n");
+#endif /* !_WIN32 */
 #endif /* !_WIN32 || _DLL */
   return EXIT_SUCCESS;
 }
--- src/pvn_fmt.h
+++ src/pvn_fmt.h
@@ -5,6 +5,62 @@
 #error pvn_fmt.h not intended for direct inclusion
 #endif /* !PVN_H */
 
+/* see https://fortranwiki.org/fortran/show/ansi_colors */
+
+#ifndef PVN_ANSI_BLACK
+#define PVN_ANSI_BLACK "\e[30m"
+#else /* PVN_ANSI_BLACK */
+#error PVN_ANSI_BLACK already defined
+#endif /* ?PVN_ANSI_BLACK */
+
+#ifndef PVN_ANSI_RED
+#define PVN_ANSI_RED "\e[31m"
+#else /* PVN_ANSI_RED */
+#error PVN_ANSI_RED already defined
+#endif /* ?PVN_ANSI_RED */
+
+#ifndef PVN_ANSI_GREEN
+#define PVN_ANSI_GREEN "\e[32m"
+#else /* PVN_ANSI_GREEN */
+#error PVN_ANSI_GREEN already defined
+#endif /* ?PVN_ANSI_GREEN */
+
+#ifndef PVN_ANSI_YELLOW
+#define PVN_ANSI_YELLOW "\e[33m"
+#else /* PVN_ANSI_YELLOW */
+#error PVN_ANSI_YELLOW already defined
+#endif /* ?PVN_ANSI_YELLOW */
+
+#ifndef PVN_ANSI_BLUE
+#define PVN_ANSI_BLUE "\e[34m"
+#else /* PVN_ANSI_BLUE */
+#error PVN_ANSI_BLUE already defined
+#endif /* ?PVN_ANSI_BLUE */
+
+#ifndef PVN_ANSI_MAGENTA
+#define PVN_ANSI_MAGENTA "\e[35m"
+#else /* PVN_ANSI_MAGENTA */
+#error PVN_ANSI_MAGENTA already defined
+#endif /* ?PVN_ANSI_MAGENTA */
+
+#ifndef PVN_ANSI_CYAN
+#define PVN_ANSI_CYAN "\e[36m"
+#else /* PVN_ANSI_CYAN */
+#error PVN_ANSI_CYAN already defined
+#endif /* ?PVN_ANSI_CYAN */
+
+#ifndef PVN_ANSI_WHITE
+#define PVN_ANSI_WHITE "\e[37m"
+#else /* PVN_ANSI_WHITE */
+#error PVN_ANSI_WHITE already defined
+#endif /* ?PVN_ANSI_WHITE */
+
+#ifndef PVN_ANSI_CLEAR
+#define PVN_ANSI_CLEAR "\e[0m"
+#else /* PVN_ANSI_CLEAR */
+#error PVN_ANSI_CLEAR already defined
+#endif /* ?PVN_ANSI_CLEAR */
+
 PVN_EXTERN_C size_t pvn_atoz(const char *const s);
 PVN_EXTERN_C char *pvn_stoa(char *const s, const float x);
 PVN_EXTERN_C char *pvn_dtoa(char *const s, const double x);
