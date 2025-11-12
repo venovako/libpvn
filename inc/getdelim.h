1961fead97fa2cf8fcb5592f981528b015f42ffd
Vedran Novakovic <venovako@venovako.eu>
2025-11-12 19:29:27 +0100

    further updates

--- inc/getdelim.h
+++ inc/getdelim.h
@@ -3,9 +3,13 @@
 #define GETDELIM_H
 
 #include "pvn_ext.h"
-
 #include <stdio.h>
 #include <stdlib.h>
+#ifdef _WIN32
+#ifndef ssize_t
+#define ssize_t long long
+#endif /* !ssize_t */
+#endif /* _WIN32 */
 
 PVN_EXTERN_C ssize_t getdelim(char **restrict linep, size_t *restrict linecapp, int delimiter, FILE *restrict stream);
 PVN_EXTERN_C ssize_t getline(char **restrict linep, size_t *restrict linecapp, FILE *restrict stream);
