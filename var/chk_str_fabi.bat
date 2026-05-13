d5605c0d12828e1ad9ab2ad8b3fb5054044bb338
Vedran Novakovic <venovako@venovako.eu>
2026-05-13 20:38:38 +0200

    further updates

--- var/chk_str_fabi.bat
+++ var/chk_str_fabi.bat
@@ -1,4 +1,4 @@
-icx /nologo /c chk_str_fabi.c /Fochk_str_fabi.obj /MT
-ifx /nologo chk_str_fabi.F90 chk_str_fabi.obj /Fechk_str_fabi.exe /MT
+icx /nologo /c chk_str_fabi.c /Fochk_str.obj /MT
+ifx /nologo chk_str_fabi.F90 chk_str.obj /Fechk_str_fabi.exe /MT
 chk_str_fabi.exe
-DEL /F chk_str_fabi.exe chk_str_fabi.obj
+DEL /F chk_str_fabi.exe chk_str.obj
