3dbab3acaf2ea5b75bcbe908ac8e5a1699b85207
Vedran Novakovic <venovako@venovako.eu>
2026-06-03 23:53:04 +0200

    further updates

---
+++ var/pvn_det.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo '"RUN", "GENns", "DDETns", "RELERRns", "INFS", "MINRELERR", "MAXRELERR", "ZDETns", "ERR"' > pvn_det.csv
+for ((R=1;R<=$1;++R))
+do
+	printf '%2d' $R >> pvn_det.csv
+	echo -n ',' >> pvn_det.csv
+	../src/pvn_det.exe 1073741824 256 >> pvn_det.csv
+done
+unset R
