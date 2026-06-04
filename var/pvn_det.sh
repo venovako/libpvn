a2ddc963fbc00895613d510b83f2aae3c7e97167
Vedran Novakovic <venovako@venovako.eu>
2026-06-05 01:27:59 +0200

    further updates

--- var/pvn_det.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Usage: ./pvn_det.sh RUNS
-echo '"RUN", "N", "GENns", "DDETns", "RELERRns", "INFS", "MINRELERReps", "MAXRELERReps", "ZDETns", "ERR"' > pvn_det.csv
-for ((R=1;R<=$1;++R))
-do
-	printf '%2d, ' $R >> pvn_det.csv
-	../src/pvn_det.exe 1073741824 256 >> pvn_det.csv
-done
-unset R
