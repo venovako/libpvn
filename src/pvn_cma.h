aa023754eb66c17412b3346a32c17d9759083f04
Vedran Novakovic <venovako@venovako.eu>
2026-09-20 20:53:03 +0200

    further updates

--- src/pvn_cma.h
+++ src/pvn_cma.h
@@ -6,51 +6,48 @@
 #endif /* !PVN_H */
 
 /* C = A * B */
-/* should be correctly rounded */
+/* rounding to nearest-even is assumed */
 
 static inline void pvn_cmul(float *const cr, float *const ci, const float ar, const float ai, const float br, const float bi)
 {
   PVN_ASSERT(cr);
   PVN_ASSERT(ci);
-  const double
-    ar_ = (double)ar,
-    ai_ = (double)ai,
-    br_ = (double)br,
-    bi_ = (double)bi,
-    cr_ = __builtin_fma(ar_, br_, -ai_ * bi_),
-    ci_ = __builtin_fma(ar_, bi_,  ai_ * br_);
-  *cr = (float)cr_;
-  *ci = (float)ci_;
+  float x
+#ifndef NDEBUG
+    = 0.0f
+#endif /* !NDEBUG */
+    ;
+  int t
+#ifndef NDEBUG
+    = 0
+#endif /* !NDEBUG */
+    ;
+  *ci = -ai;
+  *cr = PVN_FABI(pvn_sdet,PVN_SDET)(&ar, &ai, &bi, &br, &x, &t);
+  *ci = PVN_FABI(pvn_sdet,PVN_SDET)(&ar,  ci, &br, &bi, &x, &t);
 }
 
 static inline void pvn_zmul(double *const cr, double *const ci, const double ar, const double ai, const double br, const double bi)
 {
   PVN_ASSERT(cr);
   PVN_ASSERT(ci);
-#ifdef PVN_QUADMATH
-  const __float128
-    ar_ = (__float128)ar,
-    ai_ = (__float128)ai,
-    br_ = (__float128)br,
-    bi_ = (__float128)bi,
-    cr_ = fmaq(ar_, br_, -ai_ * bi_),
-    ci_ = fmaq(ar_, bi_,  ai_ * br_);
-#else /* !PVN_QUADMATH */
-  const long double
-    ar_ = (long double)ar,
-    ai_ = (long double)ai,
-    br_ = (long double)br,
-    bi_ = (long double)bi,
-    cr_ = __builtin_fmal(ar_, br_, -ai_ * bi_),
-    ci_ = __builtin_fmal(ar_, bi_,  ai_ * br_);
-#endif /* ?PVN_QUADMATH */
-  *cr = (double)cr_;
-  *ci = (double)ci_;
+  double x
+#ifndef NDEBUG
+    = 0.0
+#endif /* !NDEBUG */
+    ;
+  int t
+#ifndef NDEBUG
+    = 0
+#endif /* !NDEBUG */
+    ;
+  *ci = -ai;
+  *cr = PVN_FABI(pvn_ddet,PVN_DDET)(&ar, &ai, &bi, &br, &x, &t);
+  *ci = PVN_FABI(pvn_ddet,PVN_DDET)(&ar,  ci, &br, &bi, &x, &t);
 }
 
 /* D = A * B + C */
 /* similar to the CUDA's complex FMA from cuComplex.h */
-/* possibly not correctly rounded */
 
 static inline void pvn_cfma(float *const dr, float *const di, const float ar, const float ai, const float br, const float bi, const float cr, const float ci)
 {
@@ -98,21 +95,23 @@ static inline void pvn_zfma(double *const dr, double *const di, const double ar,
   *di = (double)di_;
 }
 
-/* COMPLEX MULTIPLICATION AND FMA OPERATIONS THAT */
-/* !!! DO NOT YET AVOID UNNECESSARY OVERFLOWS !!! */
-/* TO BE USED WHEN ALL PARTIAL RESULTS ARE NORMAL */
-
 static inline void pvn_wmul(long double *const cr, long double *const ci, const long double ar, const long double ai, const long double br, const long double bi)
 {
   PVN_ASSERT(cr);
   PVN_ASSERT(ci);
-#ifdef PVN_QUADMATH
-  *cr = fmaq(ar, br, -ai * bi);
-  *ci = fmaq(ar, bi,  ai * br);
-#else /* !PVN_QUADMATH */
-  *cr = __builtin_fmal(ar, br, -ai * bi);
-  *ci = __builtin_fmal(ar, bi,  ai * br);
-#endif /* ?PVN_QUADMATH */
+  long double x
+#ifndef NDEBUG
+    = 0.0L
+#endif /* !NDEBUG */
+    ;
+  int t
+#ifndef NDEBUG
+    = 0
+#endif /* !NDEBUG */
+    ;
+  *ci = -ai;
+  *cr = PVN_FABI(pvn_xdet,PVN_XDET)(&ar, &ai, &bi, &br, &x, &t);
+  *ci = PVN_FABI(pvn_xdet,PVN_XDET)(&ar,  ci, &br, &bi, &x, &t);
 }
 
 static inline void pvn_wfma(long double *const dr, long double *const di, const long double ar, const long double ai, const long double br, const long double bi, const long double cr, const long double ci)
@@ -133,8 +132,19 @@ static inline void pvn_ymul(__float128 *const cr, __float128 *const ci, const __
 {
   PVN_ASSERT(cr);
   PVN_ASSERT(ci);
-  *cr = fmaq(ar, br, -ai * bi);
-  *ci = fmaq(ar, bi,  ai * br);
+  __float128 x
+#ifndef NDEBUG
+    = 0.0q
+#endif /* !NDEBUG */
+    ;
+  int t
+#ifndef NDEBUG
+    = 0
+#endif /* !NDEBUG */
+    ;
+  *ci = -ai;
+  *cr = PVN_FABI(pvn_qdet,PVN_QDET)(&ar, &ai, &bi, &br, &x, &t);
+  *ci = PVN_FABI(pvn_qdet,PVN_QDET)(&ar,  ci, &br, &bi, &x, &t);
 }
 
 static inline void pvn_yfma(__float128 *const dr, __float128 *const di, const __float128 ar, const __float128 ai, const __float128 br, const __float128 bi, const __float128 cr, const __float128 ci)
@@ -149,8 +159,19 @@ static inline void pvn_ymul(long double *const cr, long double *const ci, const 
 {
   PVN_ASSERT(cr);
   PVN_ASSERT(ci);
-  *cr = __builtin_fmal(ar, br, -ai * bi);
-  *ci = __builtin_fmal(ar, bi,  ai * br);
+  long double x
+#ifndef NDEBUG
+    = 0.0L
+#endif /* !NDEBUG */
+    ;
+  int t
+#ifndef NDEBUG
+    = 0
+#endif /* !NDEBUG */
+    ;
+  *ci = -ai;
+  *cr = PVN_FABI(pvn_xdet,PVN_XDET)(&ar, &ai, &bi, &br, &x, &t);
+  *ci = PVN_FABI(pvn_xdet,PVN_XDET)(&ar,  ci, &br, &bi, &x, &t);
 }
 
 static inline void pvn_yfma(long double *const dr, long double *const di, const long double ar, const long double ai, const long double br, const long double bi, const long double cr, const long double ci)
